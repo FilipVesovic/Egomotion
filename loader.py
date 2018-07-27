4c24e6f89d2a0deb68dc627f85e37670689105a3
FilipVesovic <fv@filipvesovic.com>
2018-07-27 15:04:59 +0200

    Fix

--- loader.py
+++ loader.py
@@ -33,7 +33,7 @@ class Loader:
 
         shuffle(self.training_dataset)
 
-        valid_split = len(int(self.training_dataset * 0.8))
+        valid_split = len(int(self.training_dataset.shape[0] * 0.8))
         self.training_dataset = self.training_dataset[:valid_split]
         self.validation_dataset = self.validation_dataset[valid_split:]
 
