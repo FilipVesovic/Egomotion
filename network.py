b0b5555fb8e7f5dc669940e2fa686f73878fea30
dkutlesic <dubravka97@gmail.com>
2018-07-28 21:29:12 +0200

    fix

--- network.py
+++ network.py
@@ -4,17 +4,17 @@ import tensorflow as tf
 EPS = 0.0001
 
 def get_graph(x, is_training):
-    conv1 = tf.layers.conv2d(inputs=x, filters=32, kernel_size=[7, 7], strides=1, padding='SAME', name='conv1',kernel_initializer=tf.contrib.layers.xavier_initializer(), kernel_regularizer = tf.contrib.layers.l2_regularizer(scope = EPS) )
+    conv1 = tf.layers.conv2d(inputs=x, filters=32, kernel_size=[7, 7], strides=1, padding='SAME', name='conv1',kernel_initializer=tf.contrib.layers.xavier_initializer(), kernel_regularizer = tf.contrib.layers.l2_regularizer(scale = EPS) )
     relu1 = tf.nn.relu(conv1, name='relu1')
     pool1 = tf.layers.max_pooling2d(inputs=relu1, pool_size=[2, 2], strides=2, padding='VALID', name='pool1')
     bn1 = tf.layers.batch_normalization(pool1, training = is_training, name= 'bn1')
 
-    conv2 = tf.layers.conv2d(inputs=bn1, filters=32, kernel_size=[3, 3], strides=1, padding='SAME', name='conv2',kernel_initializer=tf.contrib.layers.xavier_initializer(), kernel_regularizer = tf.contrib.layers.l2_regularizer(scope = EPS))
+    conv2 = tf.layers.conv2d(inputs=bn1, filters=32, kernel_size=[3, 3], strides=1, padding='SAME', name='conv2',kernel_initializer=tf.contrib.layers.xavier_initializer(), kernel_regularizer = tf.contrib.layers.l2_regularizer(scale = EPS))
     relu2 = tf.nn.relu(conv2, name='relu2')
     pool2 = tf.layers.max_pooling2d(inputs=relu2, pool_size=[2, 2], strides=2, padding='VALID', name='pool2')
     bn2 = tf.layers.batch_normalization(pool2, training=is_training,name= 'bn2')
 
-    conv3 = tf.layers.conv2d(inputs=bn2, filters=32, kernel_size=[3, 3], strides=1, padding='SAME', name='conv3',kernel_initializer=tf.contrib.layers.xavier_initializer(), kernel_regularizer = tf.contrib.layers.l2_regularizer(scope = EPS))
+    conv3 = tf.layers.conv2d(inputs=bn2, filters=32, kernel_size=[3, 3], strides=1, padding='SAME', name='conv3',kernel_initializer=tf.contrib.layers.xavier_initializer(), kernel_regularizer = tf.contrib.layers.l2_regularizer(scale = EPS))
     relu3 = tf.nn.relu(conv3, name='relu3')
     pool3 = tf.layers.max_pooling2d(inputs=relu3, pool_size=[2, 2], strides=2, padding='VALID', name='pool3')
     bn3 = tf.layers.batch_normalization(pool3, training=is_training, name= 'bn3')
